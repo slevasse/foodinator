5cb332774f6a9a17232c147f26edddb156721223
Swann Levasseur <swann.levasseur@cern.ch>
2020-10-31 21:02:57 +0100

    Added some popup and restructured the main window into a class

--- food_list.py
+++ food_list.py
@@ -2,6 +2,8 @@
 
 import recipe
 import json
+import os.path
+
 
 class food_list:
     """
@@ -18,8 +20,35 @@ class food_list:
     def store_food_list(self):
         json.dump(self._recipe_list, open(self._recipe_path, 'w'), sort_keys=True)
 
-    def import_food_list(self):
-        self._recipe_list = json.load(open(self._recipe_path))
+    def import_recipe_list(self):
+        # check if the path exist
+        if not(os.path.isfile(self._recipe_path)):
+            # if it does not exist we create it
+            with open(self._recipe_path, 'w') as fp:
+                pass
+            # close it
+            fp.close()
+        else:
+            # we try to import the file
+            try:
+                self._recipe_list = json.load(open(self._recipe_path))
+            except json.decoder.JSONDecodeError:
+                print("Food list is empty or invalid")
+
+    def import_ingredient_list(self):
+        # check if the path exist
+        if not(os.path.isfile(self._ingredient_path)):
+            # if it does not exist we create it
+            with open(self._ingredient_path, 'w') as fp:
+                pass
+            # close it
+            fp.close()
+        else:
+            # we try to import
+            try:
+                self._ingredient_list = json.load(open(self._ingredient_path))
+            except json.decoder.JSONDecodeError:
+                print("Ingredient list is empty or invalid")
 
     def add_recipe(self, recipe):
         self._recipe_list.append(recipe)
@@ -31,11 +60,17 @@ class food_list:
     def store_ingredient_list(self):
         json.dump(self._ingredient_list, open(self._recipe_path, 'w'), sort_keys=True)
 
-    def set_db_path(self, path):
+    def set_recipe_db_path(self, path):
         self._recipe_path = path
 
-    def get_db_path(self):
+    def get_recipe_db_path(self):
         return self._recipe_path
+
+    def set_ingredient_db_path(self, path):
+        self._ingredient_path = path
+
+    def get_ingredient_db_path(self):
+        return self._ingredient_path
 
     def _update_recipe_count(self):
         self.recipe_count = len(self._recipe_list)
--- main.py
+++ main.py
@@ -7,39 +7,53 @@ import food_list as fl
 # get the food list object
 foodlist = fl.food_list()
 
-# define signal slot interaction here
-def on_add_recipe_button_clicked():
-    print(foodlist.recipe_count)
+class myMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main_window.ui', self)
+        self.new_ingredient_popup = None
+        self.existing_ingredient_popup = None
+        #add recipe button
+        self.pb_add_recipe = self.pushButton_add_recipe
+        self.pb_add_recipe.clicked.connect(self.on_add_recipe_button_clicked)
 
-# define signal slot interaction here
-def on_add_new_ingredient_button_clicked():
-    pass
+        self.pb_add_new_ingredient = self.pushButton_addnew_ingredient
+        self.pb_add_new_ingredient.clicked.connect(self.on_add_new_ingredient_button_clicked)
 
-# define signal slot interaction here
-def on_add_existing_ingredient_button_clicked():
-    pass
+        self.pb_add_existing_ingredients = self.pushButton_add_existing_ingredients
+        self.pb_add_existing_ingredients.clicked.connect(self.on_add_existing_ingredient_button_clicked)
+
+    def on_add_recipe_button_clicked(self):
+        print(foodlist.recipe_count)
+
+    # define signal slot interaction here
+    def on_add_new_ingredient_button_clicked(self):
+        # get a popup
+        self.new_ingredient_popup = QWidget()
+        uic.loadUi('add_ingredient_popup.ui', self.new_ingredient_popup)
+        self.new_ingredient_popup.show()
+
+    # define signal slot interaction here
+    def on_add_existing_ingredient_button_clicked(self):
+        # get a popup
+        self.existing_ingredient_popup = QWidget()
+        uic.loadUi('add_existing_ingredient.ui', self.existing_ingredient_popup)
+        self.existing_ingredient_popup.show()
+
 
 def main():
     """ Main program """
 
     # load the ui
     app = QApplication([])
-    window = QMainWindow()
-    uic.loadUi('main_window.ui', window)
-
-    #add recipe button
-    pb_add_recipe = window.pushButton_add_recipe
-    pb_add_recipe.clicked.connect(on_add_recipe_button_clicked)
-
-    pb_add_new_ingredient = window.pushButton_addnew_ingredient
-    pb_add_new_ingredient.clicked.connect(on_add_new_ingredient_button_clicked)
-
-    pb_add_existing_ingredients = window.pushButton_add_existing_ingredients
-    pb_add_existing_ingredients.clicked.connect(on_add_existing_ingredient_button_clicked)
+    #window = QMainWindow()
+    window = myMainWindow()
 
     # load the recipe list
-    foodlist.set_db_path('recipe_list_db.json')
-    foodlist.import_food_list()
+    foodlist.set_recipe_db_path('recipe_list_db.json')
+    foodlist.set_ingredient_db_path('ingredient_list_db.json')
+    foodlist.import_recipe_list()
+    foodlist.import_ingredient_list()
 
     # run the gui
     window.show()
